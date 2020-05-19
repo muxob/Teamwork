e554e3ee1b54465395f391c257bef0b517078905
Iliyan Mihov <ilian.mihov@gmail.com>
2020-05-19 17:29:01 +0300

    Setup component navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Teamwork';
+  isLoggedIn = false;
+  username: string;
+
+  logout() {
+    //this.tokenStorageService.signOut();
+  }
 }
